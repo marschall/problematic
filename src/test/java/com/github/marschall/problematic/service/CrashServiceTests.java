37a0f70594f5605026cb4d6bdd78e3cdc89bf1ea
Philippe Marschall <philippe.marschall@gmail.com>
2025-12-14 10:10:34 +0100

    test requesting memory

--- src/test/java/com/github/marschall/problematic/service/CrashServiceTests.java
+++ src/test/java/com/github/marschall/problematic/service/CrashServiceTests.java
@@ -2,6 +2,8 @@ package com.github.marschall.problematic.service;
 
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
+import java.lang.foreign.MemorySegment;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -22,6 +24,12 @@ class CrashServiceTests {
   @Test
   void crash2() {
     assertThrows(OutOfMemoryError.class, this.service::crash2);
+  }
+
+  @Test
+  void requestAndReleaseMemory() {
+    MemorySegment segment = CrashService.requestMemory();
+    CrashService.releaseMemory(segment);
   }
 
 }
